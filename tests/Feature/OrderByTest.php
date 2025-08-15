ce3112b4dbf622d8f87b0d7678160e1fde87996c
Tycho Kamphuis <tycho@aqqo.com>
2025-08-15 09:21:00 +0200

    Removed bs test

--- tests/Feature/OrderByTest.php
+++ tests/Feature/OrderByTest.php
@@ -15,15 +15,4 @@ it('Runs orderby', function (?string $orderby, string $result) {
     "Order by test1 desc, test2 desc" => ["test1 desc, test2 desc", 'select * from "test_models" order by "test1" desc, "test2" desc limit 100 offset 0'],
 ]);
 
-it('Runs orderby in expand', function () {
-    $query = createQueryFromParams(expand: 'RelatedModel($orderby=name asc)');
-    $sql = $query->toSql();
-    
-    // Debug: Let's see what the actual SQL looks like
-    dump($sql);
-    
-    // The SQL should contain an ORDER BY clause for the related model
-    expect($sql)->toContain('order by "name" asc');
-});
-
 
